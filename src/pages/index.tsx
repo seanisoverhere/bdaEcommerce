4f6423c84243b0a90ab014e8fcefe5641492898b
Sean Choon <sean_choon@hotmail.com>
2022-10-27 21:17:56 +0800

    downgrade nextjs version

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Big Data E-commerce</title>
+        <meta name="description" content="BDE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      Hello World
+    </div>
+  )
+}
