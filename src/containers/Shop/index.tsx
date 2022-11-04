3a8420d8f69b6f6bd4279b235028ab060097c02f
Sean Choon <sean_choon@hotmail.com>
2022-11-04 14:07:48 +0800

    add categories

--- src/containers/Shop/index.tsx
+++ src/containers/Shop/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { ShopContainer } from "./styles";
+import {
+  CategoryContainer,
+  CategoryText,
+  ShopContainer,
+  ShopTitle,
+} from "./styles";
 
 const Shop = () => {
-  return <ShopContainer>Test test</ShopContainer>;
+  return (
+    <ShopContainer>
+      <ShopTitle>CATALOG</ShopTitle>
+      <CategoryContainer>
+        <CategoryText $isActive>NEW</CategoryText>
+        <CategoryText>POPULAR</CategoryText>
+        <CategoryText>SHIRTS</CategoryText>
+        <CategoryText>JEANS</CategoryText>
+        <CategoryText>GARMENTS</CategoryText>
+      </CategoryContainer>
+    </ShopContainer>
+  );
 };
 
 export default Shop;
--- src/containers/Shop/styles.ts
+++ src/containers/Shop/styles.ts
@@ -3,3 +3,21 @@ import styled from "styled-components";
 export const ShopContainer = styled.div`
   margin: 9rem 5rem 2rem;
 `;
+
+export const ShopTitle = styled.div`
+  font-weight: 600;
+  font-size: 3rem;
+`;
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  overflow-x: auto;
+`;
+
+export const CategoryText = styled.div<{ $isActive?: boolean }>`
+  font-weight: 600;
+  font-size: 8rem;
+  padding-right: 3rem;
+  color: ${({ $isActive }) => ($isActive ? "#000" : "#DEDEDE")};
+  cursor: pointer;
+`;
