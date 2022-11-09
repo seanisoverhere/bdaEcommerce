caba0a2792a36aa248b1e13d21b3fc42bf2957c0
Sean Choon <sean_choon@hotmail.com>
2022-11-09 23:49:43 +0800

    style checkout

--- src/containers/Checkout/index.tsx
+++ src/containers/Checkout/index.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import {
+  CheckoutContainer,
+  CheckoutTitle,
+  FlexContainer,
+  FormWidth,
+  ItemWidth,
+} from "./styles";
+import { useAtom } from "jotai";
+import { cartAtom } from "@/store/cart";
+import {
+  ItemDetails,
+  MiniImage,
+  TextContainer,
+  Title,
+} from "@/components/Navigation/styles";
+import { Divider, Space } from "antd";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const [cart] = useAtom(cartAtom);
+
+  return (
+    <CheckoutContainer>
+      <CheckoutTitle>CHECKOUT</CheckoutTitle>
+      {cart.length > 0 ? (
+        <FlexContainer>
+          <FormWidth>Some form</FormWidth>
+          <ItemWidth style={{ alignItems: "end" }}>
+            {cart.map((item) => (
+              <>
+                <Space direction="vertical" style={{ paddingBottom: "2rem" }}>
+                  <MiniImage src={item.article_url} />
+                  <Title>{item.prod_name} x1</Title>
+                  <span>
+                    <Title>Price: </Title>
+                    S${(Number(item.price) * 590).toFixed(2)}
+                  </span>
+                </Space>
+              </>
+            ))}
+          </ItemWidth>
+        </FlexContainer>
+      ) : (
+        <TextContainer>Your cart is empty..</TextContainer>
+      )}
+    </CheckoutContainer>
+  );
 };
 
 export default Checkout;
---
+++ src/containers/Checkout/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const CheckoutContainer = styled.div`
+  margin: 9rem 5rem 2rem;
+`;
+
+export const CheckoutTitle = styled.div`
+  font-weight: 600;
+  font-size: 3rem;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 100%;
+`;
+
+export const FormWidth = styled.div`
+  width: 70%;
+`;
+
+export const ItemWidth = styled.div`
+  display: flex;
+  justify-content: end;
+  flex-direction: column;
+  width: 30%;
+  text-align: right;
+`;
